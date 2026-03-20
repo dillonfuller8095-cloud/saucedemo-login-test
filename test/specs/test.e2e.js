43ab2146b63df6160cfe842659a05b7720bfcfc6
Dillon Fuller <yourgithubemail@example.com>
2026-03-20 12:23:57 -0600

    Added login validation tests

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,3 +1,4 @@
+import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.js'
 
 describe('Dillon SauceDemo Test', () => {
@@ -5,8 +6,20 @@ describe('Dillon SauceDemo Test', () => {
     it('should login with valid credentials', async () => {
 
         await LoginPage.open()
-
         await LoginPage.loginToApp('standard_user', 'secret_sauce')
+
+        await expect(browser).toHaveUrlContaining('inventory')
+
+    })
+
+    // 👇 ADD IT RIGHT HERE
+    it('should NOT login with bad credentials', async () => {
+
+        await LoginPage.open()
+        await LoginPage.loginToApp('locked_out_user', 'wrong_password')
+
+        const error = await $('.error-message-container')
+        await expect(error).toBeDisplayed()
 
     })
 
